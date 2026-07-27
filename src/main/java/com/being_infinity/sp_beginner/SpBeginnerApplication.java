e10c73e1b2a206231b62fa2fd7924fc7220d0a54
akshay-kumar-being-infinity <akshay@being-infinity.com>
2026-07-27 18:26:26 +0530

    setup springboot project

---
+++ src/main/java/com/being_infinity/sp_beginner/SpBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.being_infinity.sp_beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBeginnerApplication.class, args);
+	}
+
+}

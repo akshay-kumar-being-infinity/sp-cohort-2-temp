b320f82683714c8d2ee4e06bb7a8b823362645e1
akshay-kumar-being-infinity <akshay@being-infinity.com>
2026-07-28 17:58:13 +0530

    added toggle page with functionality

--- target/classes/static/index.js
+++ target/classes/static/index.js
@@ -1,2 +1,30 @@
 // alert("Hello world");
 console.log("hello from java script");
+
+var users = [
+    {
+        "name" : "John Doe",
+        "image" : "/images/john.png",
+        "gender" : "Male"
+    },
+    {
+        "name" : "Jane Doe",
+        "image" : "/images/jane.png",
+        "gender" : "Female"
+    }
+]
+
+var id = 0;
+
+function toggleUser(){
+    var userName = document.getElementById("user-name");
+    var userGender = document.getElementById("user-gender");
+    var userImage = document.getElementById("user-image");
+
+
+    id = ( id + 1 ) % users.length;
+
+    userName.innerHTML = users[id].name;
+    userGender.innerHTML = users[id].gender;
+    userImage.src = users[id].image;
+}
